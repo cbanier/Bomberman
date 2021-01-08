54d0d85ad265f5fd377cebd0b60232aa80927c16
Alexandre Bousquet <alexandre.bousquet@etu.u-bordeaux.fr>
2021-01-08 18:37:12 +0100

    Premiers pas pour les bombes plus qu'a utiliser une liste de bombe et spriteBomb pour finir bg

--- src/main/java/fr/ubx/poo/model/go/Bombs.java
+++ src/main/java/fr/ubx/poo/model/go/Bombs.java
@@ -1,16 +1,31 @@
 package fr.ubx.poo.model.go;
 
 
-import fr.ubx.poo.game.Game;
+import fr.ubx.poo.model.decor.*;
+import fr.ubx.poo.game.Direction;
 import fr.ubx.poo.game.Position;
+import fr.ubx.poo.model.Movable;
+import fr.ubx.poo.model.go.GameObject;
+import fr.ubx.poo.game.Game;
+
 
 
 public class Bombs extends GameObject{
     private int stateBomb;
+    private int range;
 
     public Bombs(Game game, Position position){
         super(game,position);
         this.stateBomb=0;
+        range=game.getPlayer().getBombRange();
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
     }
 
     @Override
@@ -30,37 +45,35 @@ public class Bombs extends GameObject{
         super.setPosition(position);
     }
 
-    /*
-    // EQUIVALENT DE DOMOVE POUR LES MOVABLE
-    // TO DO ++++++
-    public void doExplosion(long state) {
-
-    }
-    //determine in function of bombRange what's can be destroyed arround the bomb
-    public boolean canDestroy(Direction direction) {
-        Position posArroundBomb_range1 = direction.nextPosition(getBombToExplosePos());
-        Position posArroundBomb_range2 = direction.nextPosition(posArroundBomb_range1);
-        if (getBombRange()>1){
-            if (game.getWorld().get(posArroundBomb_range1) instanceof Box){
-                return true;
-            }
-            else if (game.getWorld().get(posArroundBomb_range2) instanceof Box){
-                return true;
-            }
-        }
-        else {
-            if (game.getWorld().get(posArroundBomb_range1) instanceof Box){
-                return true;
-            }
-        }
-        return false;
-    }
     //make the destruction
     public void doDestroy() {
         for (Direction d : Direction.values()){
-            if (canDestroy(d)){
-                //effacer l'élément en question
+            for (int i= 1 ; i <= this.range ; i++){
+                Position nextPos= d.nextPosition(this.getPosition(),i);
+                Position next= d.nextPosition(nextPos,i);
+                if (game.getWorld().get(nextPos) instanceof Box ||  game.getWorld().get(nextPos) instanceof BombNumberDec || 
+                 game.getWorld().get(nextPos) instanceof BombNumberInc || game.getWorld().get(nextPos) instanceof BombRangeDec || 
+                 game.getWorld().get(nextPos) instanceof BombNumberInc) {
+                    game.getWorld().clear(nextPos);
+                    game.getWorld().setChanged();
+                    if (game.getPlayer().getPosition().equals(next) ||  game.getWorld().getMonsters().stream().anyMatch(monster -> monster.getPosition().equals(next))){
+                        break;
+                    }
+                }
+                if (game.getPlayer().getPosition().equals(nextPos)) {
+                    game.getPlayer().setLives(game.getPlayer().getLives()-1);
+                }
+                if (game.getWorld().getMonsters().stream().anyMatch(monster -> monster.getPosition().equals(nextPos))) {
+                    game.getWorld().getMonsters().removeIf(monster -> monster.getPosition().equals(nextPos));
+                    game.getWorld().setChanged();
+                    if (game.getPlayer().getPosition().equals(nextPos)) {
+                        game.getPlayer().setLives(game.getPlayer().getLives()-1);
+                        break;
+                    }
+                }
+                if (!game.getWorld().isEmpty(nextPos)) {
+                }
             }
         }
-    } */
+    } 
 }
