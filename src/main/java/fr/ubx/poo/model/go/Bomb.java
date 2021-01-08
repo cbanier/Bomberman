6c41942e050febc8dde00616c76b28f949cdc39a
Corentin Banier <corentin.banier@etu.u-bordeaux.fr>
2021-01-08 22:25:55 +0100

    modification nom class Bombs to Bomb

--- src/main/java/fr/ubx/poo/model/go/Bomb.java
+++ src/main/java/fr/ubx/poo/model/go/Bomb.java
@@ -1,46 +1,75 @@
 package fr.ubx.poo.model.go;
 
 
+import fr.ubx.poo.game.Direction;
 import fr.ubx.poo.game.Game;
 import fr.ubx.poo.game.Position;
+import fr.ubx.poo.model.decor.Box;
 
 
 public class Bombs extends GameObject{
-    private int stateBomb;
+    private int bombState;
+    private boolean bombRequested = false;
 
     public Bombs(Game game, Position position){
         super(game,position);
-        this.stateBomb=0;
+        this.bombState = 0;
     }
 
     @Override
     public Position getPosition() {
         return super.getPosition();
     }
-    public int getStateBomb() {
-        return stateBomb;
-    }
-
-    public void setStateBomb(int stateBomb) {
-        this.stateBomb = stateBomb;
-    }
 
     @Override
     public void setPosition(Position position) {
         super.setPosition(position);
     }
+    
+    public int getBombState() {
+        return bombState;
+    }
 
-    /*
+    public void setBombState(int bombState) {
+        this.bombState = bombState;
+    }
+
+    public void requestBomb() {
+        bombRequested = true;
+    }
+
+    public void update(long state){
+        if (bombRequested){
+            doExplosion(state);
+            //doDestroy();
+        }
+    }
+
     // EQUIVALENT DE DOMOVE POUR LES MOVABLE
     // TO DO ++++++
     public void doExplosion(long state) {
+        long lauchAt = game.getPlayer().getTimerSinceBombWasLaunch();
+        if (lauchAt - 1 == state){
 
+        }
+        if (lauchAt - 2 == state){
+
+        }
+        if (lauchAt - 3 == state){
+
+        }
+        if (lauchAt - 4 == state){
+
+        }
+        if (lauchAt - 5 == state){
+
+        }
     }
     //determine in function of bombRange what's can be destroyed arround the bomb
     public boolean canDestroy(Direction direction) {
-        Position posArroundBomb_range1 = direction.nextPosition(getBombToExplosePos());
+        Position posArroundBomb_range1 = direction.nextPosition(this.getPosition());
         Position posArroundBomb_range2 = direction.nextPosition(posArroundBomb_range1);
-        if (getBombRange()>1){
+        if (game.getPlayer().getBombRange()>1){
             if (game.getWorld().get(posArroundBomb_range1) instanceof Box){
                 return true;
             }
@@ -62,5 +91,5 @@ public class Bombs extends GameObject{
                 //effacer l'élément en question
             }
         }
-    } */
+    }
 }
